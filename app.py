efd9603ac1b08d8556fad3f7af603bec92e76c43
Alireza Yaghmaei <alireza.yaghmaa@gmail.com>
2023-10-27 19:55:14 +0330

    modified comments and APIs

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+# returns a text containing all PDFs.
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -18,31 +19,36 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-
+# divide text into chunks
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
+        separator="\n",             # CharacterTextSplitter will only split on separator
+        chunk_size=1000,            # It first looks for the first 1000 characters and then splits the next chunk from the closest separator
         chunk_overlap=200,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
     return chunks
 
-
+# returns a vector-store using text_chunks and defined embedding
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
+# creates a retrieval conversation using defined llm, vector-store and memory
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
+    
+    # ConversationBufferMemory from langchain is an extremely simple form of memory.
+    # it just keeps a list of chat messages in a buffer and passes those into the prompt template.
     memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+        memory_key='chat_history',          # chain and prompt will expect an input named chat_history
+        return_messages=True
+    )
+    
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
@@ -79,6 +85,9 @@ def main():
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
+        
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -96,6 +105,7 @@ def main():
                 vectorstore = get_vectorstore(text_chunks)
 
                 # create conversation chain
+                # we use st.session_state in order to stop streamlit from reloading the variables after doing an action like pressing a button
                 st.session_state.conversation = get_conversation_chain(
                     vectorstore)
 
